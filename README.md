d37140b262a8197adbfcc8b2f8969fb25df3fa19
Vladimir Kiselev <vladimir.yu.kiselev@gmail.com>
2017-07-13 09:25:17 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+## scmap paper figures
+This repository contains scripts for recreation of the figures from the scmap paper.
+
+### Required R libraries
+
+[cowplot](https://cran.r-project.org/web/packages/cowplot/index.html)  
+[scmap](https://github.com/hemberg-lab/scmap)  
+[irr](https://cran.r-project.org/web/packages/irr/index.html)  
+
+### Fig. 1
+[data_a](data/fig1a.csv)  
+[data_b](data/fig1b.csv)  
+[data_cd](data/fig1cd.csv)  
+[data_e](data/fig1e.csv)  
+[script](fig1.R)  
+[pdf](fig1.pdf)
+
+![](fig1.png)
+
+### Fig. 2
+[data_a](data/fig2a.txt)  
+[data_bcd1](data/fig2b-labs.csv)  
+[data_bcd2](data/fig2b-sims.csv)  
+[data_bcd3](data/fig2b-orig-labs.csv)  
+[script](fig2.R)  
+[pdf](fig2-all.pdf)
+
+Additional figures (__b__,__c__ and __d__) are added using Keynote.app. The Keynote file is available [here](fig2-all.key).
+
+![](fig2-all.png)
---
+++ fig1.R
@@ -0,0 +1,127 @@
+library(cowplot)
+
+cols1 <- c("#d73027", "#4575b4")
+
+f_select <- read.csv("data/fig1a.csv")
+f_select$Dataset <- factor(f_select$Dataset, levels = c("Pollen", "Baron (human)", "Macosko"))
+f_select$Features <- factor(f_select$Features, levels = c("Selected", "Other"))
+lm_fit <- unique(f_select[,5:7])
+p_a <- ggplot(f_select, aes(x = expression, y = dropouts, colour = Features)) + 
+    facet_grid(. ~ Dataset, scales = "free_x") +
+    geom_point(size = 0.7) + scale_colour_manual(values = cols1) + 
+    labs(x = "log2(Expression)", y = "log2(% of dropouts)") + 
+    geom_abline(data = lm_fit, aes(intercept = lm_intercept, slope = lm_slope)) + 
+    theme_classic(base_size = 7) +
+    theme(axis.line=element_blank(), strip.background = element_rect(colour = "white")) +
+    annotate("segment", x=-Inf, xend=Inf, y=-Inf, yend=-Inf, color = "black")+
+    annotate("segment", x=-Inf, xend=-Inf, y=-Inf, yend=Inf, color = "black")
+
+cols <- c("#d73027", "#fdae61", "#4575b4")
+
+self_proj <- read.csv("data/fig1b.csv")
+self_proj$n_features <- factor(self_proj$n_features, levels = c("0-100", "100-200", "200-500", "500-1000", "1000-2000", "2000-5000", "ALL"))
+self_proj$Features <- factor(self_proj$Features, levels = c("dropout", "HVG", "random"))
+self_proj$Method <- factor(self_proj$Method, levels = c("scmap", "SVM", "RF"))
+
+p_b <- ggplot(self_proj, aes(n_features, kappa, fill = Method)) +
+    geom_boxplot(size = 0.1, width = 0.5, position = position_dodge(width = 0.7), outlier.size = 0.2) +
+    facet_grid(. ~ Features) +
+    scale_fill_manual(values = cols) +
+    ylim(-0.4, 1) +
+    theme_classic(base_size = 7) +
+    theme(axis.line=element_blank(), 
+          strip.background = element_rect(colour = "white"),
+          axis.text.x=element_text(angle = -30, hjust = 0)) +
+    annotate("segment", x=0, xend=Inf, y=0, yend=0, color = "black") +
+    annotate("segment", x=0, xend=0, y=-Inf, yend=Inf, color = "black") +
+    labs(x = "Number of features", y = "Cohen's Kappa")
+
+pos_ctrls <- read.csv("data/fig1cd.csv")
+pos_ctrls$n_features <- factor(pos_ctrls$n_features, levels = c("0-100", "100-200", "200-500", "500-1000", "1000-2000", "2000-5000", "ALL"))
+pos_ctrls$Method <- factor(pos_ctrls$Method, levels = c("scmap", "SVM", "RF"))
+
+p_s1 <- ggplot(pos_ctrls, aes(n_features, kappa, fill = Method)) +
+    geom_boxplot(width = 0.5, position = position_dodge(width = 0.7)) +
+    facet_grid(threshold ~ .) +
+    scale_fill_manual(values = cols) +
+    theme_classic() +
+    theme(axis.line=element_blank(), 
+          strip.background = element_rect(colour = "white"),
+          axis.text.x=element_text(angle = -30, hjust = 0)) +
+    annotate("segment", x=0, xend=Inf, y=0, yend=0, color = "black") +
+    annotate("segment", x=0, xend=0, y=-Inf, yend=Inf, color = "black") +
+    labs(x = "Number of features", y = "Cohen's Kappa")
+ggsave(file = "figS1.png", w = 7, h = 10)
+
+p_s2 <- ggplot(pos_ctrls, aes(n_features, uns_rate, fill = Method)) +
+    geom_boxplot(width = 0.5, position = position_dodge(width = 0.7)) +
+    scale_fill_manual(values = cols) +
+    facet_grid(threshold ~ .) +
+    theme_classic() +
+    theme(axis.line=element_blank(), 
+          strip.background = element_rect(colour = "white"),
+          axis.text.x=element_text(angle = -30, hjust = 0)) +
+    annotate("segment", x=0, xend=Inf, y=0, yend=0, color = "black") +
+    annotate("segment", x=0, xend=0, y=-Inf, yend=Inf, color = "black") +
+    labs(x = "Number of features", y = "% of unassigned cells")
+ggsave(file = "figS2.png", w = 7, h = 10)
+
+p_1c <- ggplot(pos_ctrls[pos_ctrls$threshold == 0.7, ], aes(n_features, kappa, fill = Method)) +
+    geom_boxplot(size = 0.1, width = 0.5, position = position_dodge(width = 0.7), outlier.size = 0.2) +
+    scale_fill_manual(values = cols) +
+    theme_classic(base_size = 7) +
+    theme(axis.line=element_blank(), 
+          strip.background = element_rect(colour = "white"),
+          legend.position = "none",
+          axis.text.x=element_text(angle = -30, hjust = 0)) +
+    annotate("segment", x=0, xend=Inf, y=0, yend=0, color = "black") +
+    annotate("segment", x=0, xend=0, y=-Inf, yend=Inf, color = "black") +
+    labs(x = "Number of features", y = "Cohen's Kappa")
+
+p_1d <- ggplot(pos_ctrls[pos_ctrls$threshold == 0.7, ], aes(n_features, uns_rate, fill = Method)) +
+    geom_boxplot(size = 0.1, width = 0.5, position = position_dodge(width = 0.7), outlier.size = 0.2) +
+    scale_fill_manual(values = cols) +
+    theme_classic(base_size = 7) +
+    theme(axis.line=element_blank(), 
+          strip.background = element_rect(colour = "white"),
+          legend.position = "none",
+          axis.text.x=element_text(angle = -30, hjust = 0)) +
+    annotate("segment", x=0, xend=Inf, y=0, yend=0, color = "black") +
+    annotate("segment", x=0, xend=0, y=-Inf, yend=Inf, color = "black") +
+    labs(x = "Number of features", y = "% of unassigned cells")
+
+neg_ctrls <- read.csv("data/fig1e.csv")
+neg_ctrls$n_features <- factor(neg_ctrls$n_features, levels = c("0-100", "100-200", "200-500", "500-1000", "1000-2000", "2000-5000", "ALL"))
+neg_ctrls$Method <- factor(neg_ctrls$Method, levels = c("scmap", "SVM", "RF"))
+
+p_1e <- ggplot(neg_ctrls[neg_ctrls$threshold == 0.7, ], aes(n_features, uns_rate, fill = Method)) +
+    geom_boxplot(size = 0.1, width = 0.5, position = position_dodge(width = 0.7), outlier.size = 0.2) +
+    scale_fill_manual(values = cols) +
+    theme_classic(base_size = 7) +
+    theme(axis.line=element_blank(), 
+          strip.background = element_rect(colour = "white"),
+          legend.position = "none",
+          axis.text.x=element_text(angle = -30, hjust = 0)) +
+    annotate("segment", x=0, xend=Inf, y=0, yend=0, color = "black") +
+    annotate("segment", x=0, xend=0, y=-Inf, yend=Inf, color = "black") +
+    labs(x = "Number of features", y = "% of unassigned cells")
+
+p_s3 <- ggplot(neg_ctrls, aes(n_features, uns_rate, fill = Method)) +
+    geom_boxplot(width = 0.5, position = position_dodge(width = 0.7)) +
+    facet_grid(threshold ~ .) +
+    scale_fill_manual(values = cols) +
+    theme_classic() +
+    theme(axis.line=element_blank(), 
+          strip.background = element_rect(colour = "white"),
+          axis.text.x=element_text(angle = -30, hjust = 0)) +
+    annotate("segment", x=0, xend=Inf, y=0, yend=0, color = "black") +
+    annotate("segment", x=0, xend=0, y=-Inf, yend=Inf, color = "black") +
+    labs(x = "Number of features", y = "% of unassigned cells")
+ggsave(file = "figS3.png", w = 7, h = 10)
+
+third_row <- plot_grid(p_1c, p_1d, p_1e, nrow = 1, labels = c("c", "d", "e"))
+
+plot_grid(p_a, p_b, third_row, ncol = 1, labels = c("a", "b", NA))
+
+ggsave("fig1.png", w = 9, h = 6)
+ggsave("fig1.pdf", w = 9, h = 6)
